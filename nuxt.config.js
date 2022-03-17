3cd0b3f7134e2ea0136918bee97964bd56464bf8
Kingsley Atanang <Kingo4luv@yahoo.com>
2022-03-17 09:34:48 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'preconnect', href: 'https://fonts.googleapis.com'
+      },
+      {
+       href: 'https://fonts.gstatic.com', crossOrigin: true
+      },
+      {
+        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap'
+      }
+    ],
+    script: [{
+      src: process.env.APP_NODE_ENV === 'production' ? 'https://js.useklump.com/klump.js' : 'https://staging-js.useklump.com/klump.js',
+      ssr: false,
+      body: true
+    }]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  env: {
+    APP_CLIENT_ID: process.env.APP_CLIENT_ID,
+    APP_CLIENT_KEY: process.env.APP_CLIENT_KEY,
+    APP_CLIENT_SECRET: process.env.APP_CLIENT_SECRET
+  }, 
+
+  publicRuntimeConfig: {
+      axios: {
+        browserBaseURL: process.env.APP_API_URL,
+      }
+    },
+
+    privateRuntimeConfig: {
+      axios: {
+        baseURL: process.env.APP_API_URL,
+      }
+    },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  mode: 'jit',
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
